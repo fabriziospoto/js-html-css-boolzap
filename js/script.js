a8cd8974fdc077b31cde7d4843ab9cbda26f5f25
Fabrizio <fabriziospoto1@gmail.com>
2020-07-29 17:42:57 +0200

    Parziale 4
    Parziale 4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){  //Document ready per jQuery
 
-});
+$('#mic').click(function() {
+    manda();
+  });
+  // intercetto il tasto INVIO
+  $('#send-text').keypress(function(event) {
+    if(event.which == 13) {
+      manda();
+    }
+  });
+
+function manda(){
+
+        var valore = $('#send-text').val(); //prendo valore input
+
+        var elemento2 = $('.template .message').clone();
+        var el = elemento2.children('.mex-top');
+        el.children('.mex-text').text(valore); //Personalizzo clone
+        elemento2.addClass('sent');
+
+        $('.conversazione').append(elemento2); //aggiungo clone input
+}
