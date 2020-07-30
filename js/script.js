cb02a90736e9bdcbb4393034f02b310985878bb3
Fabrizio <fabriziospoto1@gmail.com>
2020-07-30 12:57:18 +0200

    Parziale 8 (Invio, ricezione, ora, scroll)
    Parziale 8 (Invio, ricezione, ora, scroll)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
-// RISPOSTE RANDOM
-var listaRisp = [
-    "Mi è sembrato di vedere un gatto.",
-    "Parto a metà agosto.",
-    "Non dire volgarità.",
-    "Rispondi perfavore.",
-    "Je ne parle pas italien.",
-    "Ich spreche kein Italienisch."
-];
-
-// INVIO
+//**********************INVIO
 $('#mic').click(function() {
     manda();
 });
@@ -18,7 +8,7 @@ $('#send-text').keypress(function(event) {    // intercetto il tasto INVIO
     }
 });
 
-// RICEZIONE
+//***********************RICEZIONE
 $('#mic').click(function() {
     setTimeout(risposta, 1500);
 });
@@ -31,40 +21,32 @@ $('#send-text').keypress(function(event) {    // intercetto il tasto INVIO
 //***************FUNZIONI
 function manda(){
     var valore = $('#send-text').val(); //prendo valore input
-
     var elemento2 = $('.template .message').clone();
-
     var el = elemento2.children('.mex-top');
     el.children('.mex-text').text(valore); //Personalizzo clone
     elemento2.addClass('sent');
     $('.conversazione').append(elemento2); //aggiungo clone input
     scroll();
-    // var ora = numeriRandom(0, 23);
-    // var minuti = numeriRandom(0, 60);
 
-    //$('.sent .mex-time').text(ora + '.' + minuti);
+    var datetime = new Date();  // Imposto ora
+    var ora = datetime.getHours();
+    var minutes = datetime.getMinutes();
+    $('.sent .mex-time').text(ora + '.' + minutes);
+
 }
 
 function risposta() {
-    i= 0;
-    while (i < 1) {
-        var indice = numeriRandom(0, listaRisp.length-1);
-        var elemento3 = $('.template .message').clone();
-        var el2 = elemento3.children('.mex-top');
-        el2.children('.mex-text').text(listaRisp[indice]);
-        elemento3.addClass('received');
-        $('.conversazione').append(elemento3);
-        scroll();
-        i++;
-    }
-    // var ora = numeriRandom(0, 23);
-    // var minuti = numeriRandom(0, 60);
+    var elemento3 = $('.template .message').clone();
+    var el2 = elemento3.children('.mex-top');
+    el2.children('.mex-text').text('Ok');
+    elemento3.addClass('received');
+    $('.conversazione').append(elemento3);
+    scroll();
 
-    // $('.received .mex-time').text(ora + '.' + minuti);
-}
-
-function numeriRandom(min,max){
-    return Math.floor(Math.random()*(max - min + 1) + min);
+    var datetime = new Date();  // Imposto ora
+    var ora = datetime.getHours();
+    var minutes = datetime.getMinutes();
+    $('.received .mex-time').text(ora + '.' + minutes);
 }
 
 function scroll() {
